6f7bb70025a34f63627f08d3ac1da13c163e4878
khalwat <khalwat@users.noreply.github.com>
2015-06-11 22:13:47 -0400

    Create Recaptcha_ServiceController.php
    This controller handles the most common case for wanting to use Recaptcha, the sign-up form.  Just use it like this:
    
    			<form method="post" accept-charset="UTF-8">
    			    {{ getCsrfInput() }}
    			    <input type="hidden" name="action" value="recaptcha/service/saveUser">
    
    ...and if the user passes the Recaptcha test, they are saved as a new user, the data forwarded on to 'users/saveUser'

---
+++ controllers/Recaptcha_ServiceController.php
@@ -0,0 +1,28 @@
+<?php
+namespace Craft;
+
+/**
+ * Recaptcha controller
+ */
+class Recaptcha_ServiceController extends BaseController
+{
+    protected $allowAnonymous = true;
+	/**
+	 * Handle the save user form request.
+	 */
+	public function actionSaveUser()
+	{
+        $this->requirePostRequest();
+		$captcha = craft()->request->getPost('g-recaptcha-response');
+		$verified = craft()->recaptcha_verify->verify($captcha);
+		
+		if ($verified)
+		{
+			$this->forward('users/saveUser');
+		} else {
+            craft()->urlManager->setRouteVariables(array(
+               "Failed Recaptcha validation",
+            ));
+		}
+	}
+}
