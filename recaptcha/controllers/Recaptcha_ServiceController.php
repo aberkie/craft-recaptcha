6694323f2dca253eb346ac5ef567271fe90ba390
Andrew Welch <andrew@AmbrosiaSW.com>
2015-11-26 10:56:18 -0500

    Fixed issue if required fields were not filled in

--- recaptcha/controllers/Recaptcha_ServiceController.php
+++ recaptcha/controllers/Recaptcha_ServiceController.php
@@ -7,25 +7,25 @@ namespace Craft;
 class Recaptcha_ServiceController extends BaseController
 {
     protected $allowAnonymous = true;
-	/**
-	 * Handle the save user form request.
-	 */
-	public function actionSaveUser()
-	{
+    /**
+     * Handle the save user form request.
+     */
+    public function actionSaveUser()
+    {
         $this->requirePostRequest();
-		$captcha = craft()->request->getPost('g-recaptcha-response');
-		$verified = craft()->recaptcha_verify->verify($captcha);
-		
-		if ($verified)
-		{
-			$this->forward('users/saveUser', false);
-		}
-		else
-		{
+        $captcha = craft()->request->getPost('g-recaptcha-response');
+        $verified = craft()->recaptcha_verify->verify($captcha);
+        
+        if ($verified)
+        {
+            $this->forward('users/saveUser', false);
+        }
+        else
+        {
             craft()->urlManager->setRouteVariables(array(
                'errors' => array('Failed Recaptcha validation',
                )
             ));
-		}
-	}
+        }
+    }
 }
