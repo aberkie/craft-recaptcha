edf43312de1fef0819f7460e3e0a6e4f3b566778
Christiaan Tigelaar <christiaan@sitemind.nl>
2015-12-09 13:11:34 +0100

    Enhanced error handling

--- RecaptchaPlugin.php
+++ RecaptchaPlugin.php
@@ -19,7 +19,7 @@ class RecaptchaPlugin extends BasePlugin
 
     function getVersion()
     {
-        return '1.0.0';
+        return '1.0.1';
     }
 
     function getDeveloper()
--- controllers/Recaptcha_ServiceController.php
+++ controllers/Recaptcha_ServiceController.php
@@ -16,15 +16,19 @@ class Recaptcha_ServiceController extends BaseController
         $captcha = craft()->request->getPost('g-recaptcha-response');
         $verified = craft()->recaptcha_verify->verify($captcha);
 
-        if ($verified)
-        {
+        if ($verified) {
             $this->forward('users/saveUser', false);
-        }
-        else
-        {
+        } else {
+            $user = new UserModel();
+            $user->addError('recaptcha', Craft::t("Failed reCAPTCHA validation."));
+
+            // Store given values, so the user don't has to repeat them
+            $user->username = craft()->request->getPost('username');
+            $user->email = craft()->request->getPost('email');
+
+            // Send the account back to the template
             craft()->urlManager->setRouteVariables(array(
-               'errors' => array('Failed Recaptcha validation',
-               )
+                'account' => $user
             ));
         }
     }
